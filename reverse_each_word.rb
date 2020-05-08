c3e94ed641ebcf6215eed62a0e5b7e4631ff8e31
impact learner <time.darden@gmail.com>
2020-05-08 18:49:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
+# def reverse_each_word(string)
+#   words_reversed = []
+#   words = string.split(" ")
+#   words.each{ |word|
+#     words_reversed.push(word.reverse)
+#   }
+#   words_reversed.join(" ")
+# end
+
 def reverse_each_word(string)
-  words_reversed = []
   words = string.split(" ")
-  words.each{ |word|
-    words_reversed.push(word.reverse)
-  }
+  words_reversed = words.collect{|word| word.reverse}
   words_reversed.join(" ")
 end
