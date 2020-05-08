7ca06f1d0490ac45e64c6cf49a443a1c953fe341
impact learner <time.darden@gmail.com>
2020-05-08 19:04:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-# def reverse_each_word(string)
-#   words_reversed = []
-#   words = string.split(" ")
-#   words.each{ |word|
-#     words_reversed.push(word.reverse)
-#   }
-#   words_reversed.join(" ")
-# end
+def reverse_each_word(string)
+  words_reversed = []
+  words = string.split(" ")
+  words.each{ |word|
+    words_reversed.push(word.reverse)
+  }
+  words_reversed.join(" ")
+end
 
 def reverse_each_word(string)
   words = string.split(" ") #splits each word in a phrase(based on spaces)
