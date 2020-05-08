74dfaacf8f957186d1253534fce8d65190565783
impact learner <time.darden@gmail.com>
2020-05-08 18:34:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   words_reversed = []
   words = string.split(" ")
   words.each{ |word|
-    words.reversed.push(word.reverse)
+    words_reversed.push(word.reverse)
   }
   words_reversed.join(" ")
 end
